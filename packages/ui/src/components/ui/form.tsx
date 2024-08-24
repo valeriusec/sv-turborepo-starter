2394ca395759e8258509622b7429ee99965a686c
valeriusec <valeriu.secrieru19@gmail.com>
2024-08-24 12:11:41 +0200

    ...

--- packages/ui/src/components/ui/form.tsx
+++ packages/ui/src/components/ui/form.tsx
@@ -12,8 +12,8 @@ import {
   useFormContext,
 } from "react-hook-form";
 
-import { cn } from "../../lib/utils";
-import { Label } from "../../components/ui/label";
+import { cn } from "@repo/ui/lib/utils";
+import { Label } from "@repo/ui/components/ui/label";
 
 const Form = FormProvider;
 
